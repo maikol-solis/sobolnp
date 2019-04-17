4a958049258bceb5adaaa1e3cddb778ec9fffbbf
Maikol Solís <maikol.solis@ucr.ac.cr>
2019-04-17 11:52:09 -0600

    Refactor all the sobolnp function Now the sobolnp works based in lists to use the parallel::mclapply function.

--- R/sobolnp.R
+++ R/sobolnp.R
@@ -1,365 +1,295 @@
-# Author: Maikol Solís
-#To create a robust function, I took as a template the script of Filippo Monari,
-#'sobolSmthSpl' from the package 'sensitivity'. However, the core estimations
-#are my original work.
-
 sobolnp <- function(Y,
                     X,
+                    bandwidth = NULL,
+                    bandwidth.total = NULL,
+                    bandwidth.compute = TRUE,
+                    bootstrap,
                     nboot = 100,
-                    total.indices = FALSE,
-                    bootstrap = FALSE,
-                    ckerorder = 2) {
-  #Arguments:
-  #Y: matrix of model outputs (only one column)
-  #X: matrix model parameters
-  #nboot: number of bootstrap samples
-  #bootstrap: logical if apply bootstrap or not
-  #ckerorder: Kernel order for the nonparametric estimator
-  #MAIN
+                    ckerorder = 2,
+                    mc.cores = 2) {
+  n <- length(Y)
+  p <- ncol(X)
 
-  if (!requireNamespace("np", quietly = TRUE)) {
-    stop("Please install the package np: install.packages('np')")
-  }#end-require-np
-
-  ANS = list()
-  ANS[['call']] = match.call()
-  ANS[['X']] = X
-  ANS[['Y']] = Y
-  ANS[['bootstrap']] <- bootstrap
-  if (bootstrap) {
-    ANS[['nboot']] = nboot
-  }
-  ANS[['ckerorder']] <- ckerorder
-  par.names = colnames(X)	#gets parameters names
-  if (is.null(colnames(X)))
-    par.names = paste0('X', 1:ncol(X))
-  # X = normalize(X)    		#normalize inpiuts between [0,1]
-  #Y = Y - mean(Y)     		#center model responses
-  # Y = sapply(1:ncol(X), function(i)
-  #   return(Y[order(X[, i])]))    #order Y before X (or create a new variable for X)
-  # X = sapply(1:ncol(X), function(i)
-  #   return(X[order(X[, i]), i]))
-  NP <- optNP(
-    Y,
-    X,
-    total.indices,
-    nboot = nboot,
-    bootstrap = bootstrap,
-    ckerorder = ckerorder
-  )
-  SA.tab <- t(sapply(NP, est.Si))
-  colnames(SA.tab) <-
-    c('Si', 'bw' , 'STi', paste0('bwT', 1:(ncol(X) - 1)))
-  rownames(SA.tab) = par.names
-  ANS[['S']] = SA.tab
-  class(ANS) = 'sobolnp'
-  return(ANS)
-}
-
-est.Si <- function (NP) {
-  gi <- NP[['mean']]
-  gTi <- NP[['mean.Total']]
-  bw <- NP[['bws']]
-  bwTi <- NP[['bws.Total']]
-  yi <- NP[['y']]
-  Si <- var(gi) / var(yi)
-  STi <- 1 - var(gTi) / var(yi)
-  # #calculates the standard error of the main effect estimates
-  # yi.sc = (yi - mean(yi)) / sd(yi)            	#scaled yi
-  # u = (yi - gi) / (sd(yi) * abs(1 - Si) ** 0.5)   #scales residuals
-  # Si.se = abs(1 - Si) * sd(yi.sc ** 2 - u ** 2) / length(yi) ** 0.5
-  # q0.05 = qnorm(0.05, Si, Si.se)
-  return(c(Si, bw, STi, bwTi))
-}
-
-optNP <- function (Y,
-                   X,
-                   total.indices,
-                   nboot,
-                   bootstrap,
-                   ckerorder) {
-
-  #DOC
-  #Estimates nonparametrically the regression curve across all the column of the
-  #matrix Y.
-  #Arguments:
-  #Y: matrix containing the response variable
-  #X: matrix of inputs
-  #nboot: number of bootstrap samples
-  #bootstrap: logical if apply bootstrap or not
-  #ckerorder: Kernel order for the nonparametric estimator
-
-  #CONTAINS
-
-  ghat_boot <- function(Y_boot, Xi, h, ckerorder = 2) {
-    ghat_boot <- lapply(
-      X = Y_boot,
-      FUN = function(ydat, xdat, h, ckerorder) {
-        ghat.bw <- np::npregbw(
-          xdat = xdat,
-          ydat = ydat,
-          bws = h,
-          bandwidth.compute = FALSE,
-          ckertype = "epanechnikov",
-          ckerorder = ckerorder,
-          bwmethod = "cv.ls"
-        )
-        ghat <- np::npreg(ghat.bw)
-        return(mean = ghat$mean)
-      },
-      #end-function-ghat_boot
-      xdat = Xi,
-      h = h,
-      ckerorder  = ckerorder
-    )
-    ghat_boot_mean <- apply(simplify2array(ghat_boot), 1, mean)
-    return(ghat_boot_mean)
+  if (any(bandwidth < 0)) {
+    return(list(
+      S = rep(NA, p),
+      ST = rep(NA, p),
+      bws = rep(NA, p),
+      bwsT = rep(NA, p)
+    ))
   }
 
-  objective <- function(h,
-                        Y_boot,
-                        Y,
-                        Xi,
-                        ckerorder = 2) {
-    ghat_boot_mean <- ghat_boot(
-      Y_boot = Y_boot,
-      Xi = Xi,
-      h = h,
-      ckerorder = ckerorder
-    )
-    return(mean((ghat_boot_mean - Y) ^ 2))
-  } # end-function-objetive
+  if (is.null(bandwidth)) {
+    bandwidth <- NULL
+  }
+
+  if (is.null(bandwidth.total)) {
+    bandwidth.total <- rep(NULL, p - 1)
+  }
 
 
-  estimation_mean_and_bws <-
-    function(i,
-             Ydat,
-             Xdat,
-             total.indices,
-             nboot,
-             bootstrap,
-             ckerorder) {
-      Y <- Ydat
-      X <- Xdat
-      ghat.bw <- np::npregbw(
-        xdat = X[, i],
+  SList <- pbmclapply(
+    X = 1:p,
+    FUN = function(k) {
+      compute.sobol.indices(
+        xdat = X[, k],
         ydat = Y,
-        ckertype = "epanechnikov",
-        bwmethod = "cv.ls"
+        bws = bandwidth,
+        bandwidth.compute = bandwidth.compute,
+        ckerorder = ckerorder
       )
-      ghat <- np::npreg(ghat.bw, residuals = TRUE)
+    },
+    mc.cores = mc.cores
+  )
 
-      if (total.indices == TRUE) {
-        ghat.bw.Total <- np::npregbw(
-          xdat = X[, -i],
-          ydat = Y,
-          ckertype = "epanechnikov",
-          bwmethod = "cv.ls"
-        )
-        ghat.Total <- np::npreg(ghat.bw.Total, residuals = TRUE)
-      }
+  STotalList <- pbmclapply(
+    X = 1:p,
+    FUN = function(k) {
+      x <- compute.sobol.indices(
+        xdat = X[, -k],
+        ydat = Y,
+        bws = bandwidth.total,
+        bandwidth.compute = bandwidth.compute,
+        ckerorder = ckerorder
+      )
+      x$S <- 1 - x$S
+      x$bws <-
+        append(x$bws , NA, after = k - 1)
+      return(x)
+    },
+    mc.cores = mc.cores
+  )
 
 
-      if (!bootstrap) {
-        if (total.indices == TRUE) {
-          return(
-            list(
-              mean = ghat$mean,
-              bws = ghat$bw,
-              mean.Total = ghat.Total$mean,
-              bws.Total = ghat.Total$bw,
-              x = X[, i],
-              y = Y
-            )
-          )
-        } else{
-          return(list(
-            mean = ghat$mean,
-            bws = ghat$bw,
-            x = X[, i],
-            y = Y
-          ))
-        }
-      } #end-if-!bootstrap
-      else if (bootstrap) {
-        resid.np <- np::npreg(np::npregbw(
-          xdat = X[, i],
-          ydat = residuals(ghat),
-          ckertype = "epanechnikov"
-        ))
-        g <- ghat$mean
-        sderr <- resid.np$merr
-        sderr.idx <- sderr <= 1e-5
-        error  <-
-          (residuals(ghat) - mean(residuals(ghat))) / sderr
-        error[sderr.idx] <- 0
 
-        n <- length(g)
-        Y_boot <- mapply(
-          FUN = function(n)
-          {
-            idx <- sample(1:n, replace = TRUE)
-            return(g +  sderr * error[idx])
-          },
-          # end-function-Y_boot
-          rep(n, nboot),
-          SIMPLIFY = FALSE
-        )
+  S <- sapply(SList, function(l)
+    l$S)
+  bws_initial <- sapply(SList, function(l)
+    l$bws)
+  ST <- sapply(STotalList, function(l)
+    l$S)
+  bwsT_initial <- t(sapply(STotalList, function(l)
+    l$bws))
 
-        cv.opt <- optimize(
-          f = objective,
-          interval = c(0,  2 * ghat$bw),
-          Y_boot = Y_boot,
-          Y = Y,
-          Xi = X[, i],
-          ckerorder = ckerorder
-        )
-
-        h_boot_min <- cv.opt$minimum
-
-        g_boot_min_value <-
-          ghat_boot(
-            Y_boot = Y_boot,
-            Xi = X[, i],
-            h = h_boot_min,
-            ckerorder = ckerorder
-          )
-
-        return(list(
-          mean = g_boot_min_value,
-          bws = h_boot_min,
-          x = X[, i],
-          y = Y
-        ))
-
-      } #end-if-bootstrap
-    } #end-function-estimation_mean_and_bws
-
-  nc <-  min(ncol(Y), parallel::detectCores())
-
-  ANS <- try(parallel::mclapply(
-    X = 1:ncol(X),
-    FUN = estimation_mean_and_bws,
-    Ydat = Y,
-    Xdat = X,
-    total.indices,
-    nboot = nboot,
-    bootstrap = bootstrap,
-    ckerorder = ckerorder,
-    mc.cores = nc
-  ),
-  silent = T)
-
-  if (is(ANS, 'try-error')) {
-    #Windows does not support mclapply...
-    NP <- lapply(
-      X = 1:ncol(X),
-      FUN = estimation_mean_and_bws,
-      Ydat = Y,
-      Xdat = X,
-      total.indices,
-      nboot = nboot,
-      bootstrap = bootstrap,
-      ckerorder = ckerorder
-    )
-  } else {
-    NP <<- ANS
-  }
-
-  return(NP)
-} #end-optNP
-
-normalize <- function (X,
-                       MAXS = NULL,
-                       MINS = NULL,
-                       inv = F) {
-  #DOC
-  #Normalizes a vector or a matrix according to the given 'MAXS' and 'MINS'.
-  #If 'MAXS' and 'MINS' are not provided 'X' is scaled so that each column is within [0,1].
-  #ARGUMENTS
-  #X: matrix or vector to normalize
-  #MAXS, MINS: maxima and minima. NULL or NA values are set to max(X[,i]) and min(X[,i]) respectively.
-  #inv: if T performs the inverse transformation
-  #MAIN
-  X = as.matrix(X)
-  if (inv) {
-    return(scale(
-      scale(X, center = F, scale = (MAXS - MINS) ** (-1)),
-      center = -MINS,
-      scale = F
+  if (bootstrap == FALSE) {
+    return(list(
+      S = S,
+      ST = ST,
+      bws = bws_initial,
+      bwsT = bwsT_initial
     ))
   } else {
-    if (is.null(MAXS))
-      MAXS = apply(X, 2, max)
-    if (is.null(MINS))
-      MINS = apply(X, 2, min)
-    for (i in 1:ncol(X)) {
-      if (is.na(MAXS[i]))
-        MAXS[i] = max(X[, i])
-      if (is.na(MINS[i]))
-        MINS[i] = min(X[, i])
+    meanS <- sapply(SList, function(l)
+      l$mean)
+    sderr <- sapply(SList, function(l)
+      l$sderr)
+    # meanres <- sapply(SList, function(l)
+    #   l$meanres)
+    error <- sapply(SList, function(l)
+      l$error)
+    meanT <- sapply(STotalList, function(l)
+      l$mean)
+    sderrT <- sapply(STotalList, function(l)
+      l$sderr)
+    errorT <- sapply(STotalList, function(l)
+      l$error)
+
+    Y_boot <- mapply(
+      FUN = function(n) {
+        #browser()
+        idx <-
+          matrix(sample(1:n, size = n * p, replace = TRUE), nrow = n)
+        return(meanS + sderr * error[idx])
+      },
+      rep(n, nboot),
+      SIMPLIFY = FALSE
+    )
+
+    Y_bootT <- mapply(
+      FUN = function(n) {
+        #browser()
+        idx <-
+          matrix(sample(1:n, size = n * p, replace = TRUE), nrow = n)
+        return(meanT + sderrT * errorT[idx])
+      },
+      rep(n, nboot),
+      SIMPLIFY = FALSE
+    )
+
+    Sboot <- numeric(p)
+    STboot <- numeric(p)
+    bwsboot <- numeric(p)
+    bwsSTboot <- matrix(nrow = p, ncol = p)
+    for (k in 1:p) {
+      cv_opt <-
+        bobyqa(
+          par = bws_initial[k],
+          fn = BLS,
+          xdat = X[, k],
+          ydat = Y,
+          ydat_boot = Y_boot,
+          var.index = k,
+          ckerorder = ckerorder,
+          #method = "L-BFGS-B"#,
+          lower = 0,
+          upper = 10 * bws_initial[k],
+          # method = "L-BFGS-B",
+          control = list(iprint = 2)
+        )
+
+
+      gS <- compute.sobol.indices(
+        xdat = X[, k],
+        ydat = Y ,
+        bws = cv_opt$par,
+        bandwidth.compute = FALSE,
+        ckerorder = ckerorder ,
+        only.mean = TRUE
+      )
+      Sboot[k] <- var(gS) / var(Y)
+      bwsboot[k] <-  cv_opt$par
+
+
+      cv_opt_T <-
+        bobyqa(
+          par = bwsT_initial[k, -k],
+          fn = BLS,
+          xdat = X[, -k],
+          ydat = Y,
+          ydat_boot = Y_bootT,
+          var.index = k,
+          ckerorder = ckerorder,
+          #method = "L-BFGS-B"#,
+          lower = 0,
+          upper = 10 * bwsT_initial[k, -k],
+          control = list(iprint = 2)
+        )
+
+      gT <- compute.sobol.indices(
+        xdat = X[, -k],
+        ydat = Y ,
+        bws = cv_opt_T$par,
+        bandwidth.compute = FALSE,
+        ckerorder = ckerorder ,
+        only.mean = TRUE
+      )
+      STboot[k] <- 1 - var(gT) / var(Y)
+
+
+      bwsSTboot[k, ] <-
+        append(cv_opt_T$par, NA, after = k - 1)
     }
-    return(scale(X, center = MINS, scale = MAXS - MINS))
-  }
+
+    return(
+      list(
+        S = S,
+        ST = ST,
+        bws = bws_initial,
+        bwsT = bwsT_initial,
+        Sboot = Sboot,
+        STboot = STboot,
+        bwsboot = bwsboot,
+        bwsSTboot = bwsSTboot
+      )
+    )
+  }#end-else-bootstrap
 }
 
-plot.sobolnp <- function(x, ...) {
-  yrng = range(c(1 + x[['S']][, 'se'], x[['S']][, 'q0.05']))
-  #plot estimates
-  plot(
-    x = 1:nrow(x[['S']]),
-    y = x[['S']][, 'Si'],
-    pch = 19,
-    ylim = yrng,
-    xaxt = 'n',
-    xlab = 'parameter',
-    ylab = 'Si and se',
-    ...
-  )
-  #plot q 0.05
-  points(
-    x = 1:nrow(x[['S']]),
-    y = x[['S']][, 'q0.05'],
-    pch = 19,
-    col = 2
-  )
-  #plot se
-  arrows(
-    x0 = 1:nrow(x[['S']]),
-    y0 = x[['S']][, 'Si'],
-    y1 = x[['S']][, 'Si'] - x[['S']][, 'se'],
-    length = 0
-  )	#lower
-  arrows(
-    x0 = 1:nrow(x[['S']]),
-    y0 = x[['S']][, 'Si'],
-    y1 = x[['S']][, 'Si'] + x[['S']][, 'se'],
-    length = 0
-  )	#upper
-  #plot 0
-  abline(h = 0, lty = 2)
-  #x axis
-  axis(side = 1,
-       at = 1:nrow(x[['S']]),
-       labels = row.names(x[['S']]))
-  #legend
-  legend(
-    'topright',
-    legend = c('Si', 'se', 'q0.05'),
-    pch = c(19, NA, 19),
-    lty = c(NA, 1, NA),
-    col = c(1, 1, 2),
-    horiz = T,
-    bty = 'n'
-  )
-}
 
-print.sobolnp <- function(x, ...) {
-  cat("\nCall:\n", deparse(x[['call']]), "\n", sep = "")
-  cat("\nNumber of observations:", length(x[['Y']]), "\n")
-  if (x[['bootstrap']]) {
-    cat("\nBootstrap Samples:", x[['nboot']], "\n")
+compute.sobol.indices <-
+  function(xdat,
+           ydat ,
+           bws,
+           bandwidth.compute,
+           ckerorder,
+           only.mean = FALSE) {
+    ghat <- npreg(
+      npregbw(
+        bws = bws,
+        xdat = xdat,
+        ydat = ydat,
+        bandwidth.compute = bandwidth.compute,
+        ckertype = "epanechnikov",
+        ckerorder = ckerorder,
+        bwmethod = "cv.ls"
+      ),
+      residuals = TRUE
+    )
+    g <- ghat$mean
+    if (only.mean) {
+      return(mean = g)
+    } else{
+      bws <- ghat$bw
+
+      res <- residuals(ghat)
+
+      res.np <- npreg(npregbw(
+        xdat = xdat,
+        ydat = res,
+        ckertype = "epanechnikov"
+      ))
+
+
+      sderr <- ghat$merr
+      sderr.idx <- sderr <= 1e-5
+      #meanres <- mean(res)
+      error  <- (res - mean(res)) / sderr
+      error[sderr.idx] <- 0
+
+      return(list(
+        S = var(g) / var(ydat),
+        mean = g,
+        bws = bws,
+        sderr = sderr,
+        #meanres = meanres,
+        error = error
+      ))
+    }
   }
-  cat("\nFirst order indices:\n")
-  print(x[['S']])
-}
+
+
+BLS <-
+  function(bws,
+           xdat,
+           ydat,
+           ydat_boot,
+           var.index,
+           ckerorder) {
+    #message(bws)
+
+    if (any(bws < 0) | any(is.na(bws))) {
+      return(10e10)
+    }
+
+    mean_boot <-  sapply(ydat_boot, function(yboot,
+                                             xdat,
+                                             bws,
+                                             ckerorder) {
+      g <- compute.sobol.indices(
+        xdat = xdat,
+        ydat = yboot[, var.index],
+        bws = bws,
+        bandwidth.compute = FALSE,
+        ckerorder = ckerorder,
+        only.mean = TRUE
+      )
+      return(g)
+    },
+    xdat = xdat,
+    bws = bws,
+    ckerorder)
+
+    return(mean(abs(ydat - rowMeans(mean_boot))))
+
+    # delta <- 10
+    # yerr <- ydat - rowMeans(mean_boot)
+    # loss <-
+    #   ifelse(abs(yerr) <= delta, 0.5 * yerr ^ 2, delta * (abs(yerr) - delta *                                                           0.5))
+    # print(mean(loss))
+    #     return(mean(loss))
+
+    # return(mean(log(cosh(
+    #
+    # ))))
+    # return(mean((ydat - rowMeans(mean_boot)) ^ 2))
+  }
